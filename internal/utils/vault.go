5a9450943effb9e240971d9e496a28ed34ff731d
AvranWong <avranwong@gmail.com>
2025-08-07 23:27:21 +0800

    update internal to use new types/vault

--- internal/utils/vault.go
+++ internal/utils/vault.go
@@ -5,15 +5,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"io"
-	"crypto/rand"
-	"crypto/sha256"
-	"crypto/aes"
-	"crypto/cipher"
 
 	vaultType "github.com/vultisig/commondata/go/vultisig/vault/v1"
 	"google.golang.org/protobuf/proto"
 
+	"github.com/vultisig/vultisig-go/common"
 	"github.com/vultisig/vultisig-go/internal/storage"
 )
 
@@ -121,7 +117,7 @@ func (vl *VaultLoader) LoadAndParseVault(vaultInput string, localPassword string
 		if err != nil {
 			return nil, nil, "", err
 		}
-		vaultBytes, err = DecryptVault(localPassword, vaultBytes)
+		vaultBytes, err = common.DecryptVault(localPassword, vaultBytes)
 		if err != nil {
 			return nil, nil, "", err
 		}
@@ -136,67 +132,4 @@ func (vl *VaultLoader) LoadAndParseVault(vaultInput string, localPassword string
 	}
 
 	return vault, vaultContainer, vaultPath, nil
-}
-
-func EncryptVault(password string, vault []byte) ([]byte, error) {
-	// Hash the password to create a key
-	hash := sha256.Sum256([]byte(password))
-	key := hash[:]
-
-	// Create a new AES cipher using the key
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// Use GCM (Galois/Counter Mode)
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a nonce. Nonce size is specified by GCM
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-
-	// Seal encrypts and authenticates plaintext
-	ciphertext := gcm.Seal(nonce, nonce, vault, nil)
-	return ciphertext, nil
-}
-
-func DecryptVault(password string, vault []byte) ([]byte, error) {
-	// Hash the password to create a key
-	hash := sha256.Sum256([]byte(password))
-	key := hash[:]
-
-	// Create a new AES cipher using the key
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// Use GCM (Galois/Counter Mode)
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	// Get the nonce size
-	nonceSize := gcm.NonceSize()
-	if len(vault) < nonceSize {
-		return nil, fmt.Errorf("ciphertext too short")
-	}
-
-	// Extract the nonce from the vault
-	nonce, ciphertext := vault[:nonceSize], vault[nonceSize:]
-
-	// Decrypt the vault
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return plaintext, nil
 }
