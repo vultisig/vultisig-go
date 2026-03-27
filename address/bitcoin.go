4b7e6a7a6cd24dd42997ec2f9bfd4472a97d605f
Raghav Sood <r@raghavsood.com>
2026-03-27 16:08:02 +0800

    refactor: replace btcutil in bitcoin.go with pure Go segwit encoding
    Use local hash160 and bech32 with segwit witness version 0 encoding.
    Remove btcsuite/btcd dependency from bitcoin.go.

--- address/bitcoin.go
+++ address/bitcoin.go
@@ -3,9 +3,6 @@ package address
 import (
 	"encoding/hex"
 	"fmt"
-
-	"github.com/btcsuite/btcd/btcutil"
-	"github.com/btcsuite/btcd/chaincfg"
 )
 
 func GetBitcoinAddress(hexPublicKey string) (string, error) {
@@ -13,10 +10,18 @@ func GetBitcoinAddress(hexPublicKey string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("invalid derived ECDSA public key: %w", err)
 	}
-	witnessProgram := btcutil.Hash160(pubKeyBytes)
-	conv, err := btcutil.NewAddressWitnessPubKeyHash(witnessProgram, &chaincfg.MainNetParams)
+	witnessProgram := hash160(pubKeyBytes)
+	// P2WPKH: witness version 0 + 20-byte keyhash, bech32 encoded with "bc" HRP
+	return segwitEncode("bc", 0, witnessProgram)
+}
+
+// segwitEncode encodes a segwit address with the given HRP, witness version, and program.
+func segwitEncode(hrp string, version byte, program []byte) (string, error) {
+	data, err := convertBits(program, 8, 5, true)
 	if err != nil {
-		return "", fmt.Errorf("fail to get witness public key hash: %w", err)
+		return "", fmt.Errorf("converting witness program: %w", err)
 	}
-	return conv.EncodeAddress(), nil
+	// Prepend witness version
+	data = append([]int{int(version)}, data...)
+	return bech32Encode(hrp, data), nil
 }
